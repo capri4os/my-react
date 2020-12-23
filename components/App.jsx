50d5e34aee61d073599a4a85d7edb5e1f1205562
Ekaterina Erokhina <katerina.shimshek@gmail.com>
2020-12-23 18:28:51 +0300

    lesson 1

---
+++ components/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default App = (props) => {
+    return <main>Some text</main>;
+};
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+// const App = React.createElement('main', {className: 'main'}, 'Some text');
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
